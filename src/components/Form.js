f9c7f1ad6262a2c8654bc2762e16404abbc5290f
franco <francosanchez8102@gmail.com>
2024-10-15 02:36:52 -0300

    update: structure of the web

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import Swal from 'sweetalert2'; 
+import Swal from 'sweetalert2';
 function Form({ addTask }) {
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
-  const [date, setDate] = useState('');
+  const [antelacion, setAntelacion] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,39 +12,40 @@ function Form({ addTask }) {
 
     // Validación de nombre
     if (!name.trim() || !/^[a-zA-Z\s]+$/.test(name)) {
-        errors.push('El nombre debe ser un texto válido.');
+      errors.push('El nombre debe ser un texto válido.');
     }
 
     // Validación de edad
     const ageNum = parseInt(age, 10);
-    if (!/^\d+$/.test(age) || ageNum < 0) {
-        errors.push('La edad debe ser un número entero no negativo.');
+    if (!/^\d+$/.test(age) || ageNum < 0 || ageNum > 999) {
+      errors.push(
+        'La edad debe ser un número entero no negativo de maximo tres cifras.'
+      );
     }
-
-    // Validación de fecha
-    const selectedDate = new Date(date);
-    const today = new Date();
-    //today.setHours(0, 0, 0, 0);
-    //selectedDate.setHours(0, 0, 0, 0);
-    if (selectedDate < today) {
-        errors.push('La fecha de viaje no puede ser anterior a hoy.');
+    // Validación de antelacion
+    const antelacionNum = parseInt(antelacion, 10);
+    if (antelacionNum < 0 || !/^\d+$/.test(antelacion)) {
+      errors.push('La antelación debe ser un número entero positivo.');
     }
 
     // Si hay errores, mostrar alerta
     if (errors.length > 0) {
-        Swal.fire('Error', errors.join(' '), 'error');
+      Swal.fire('Error', errors.join(' '), 'error');
     } else {
-        // Si todas las validaciones pasan, agregar el pasajero
-        addTask(name, ageNum, date);
-        Swal.fire('Éxito', 'Pasajero registrado correctamente.', 'success');
-        // Limpiar campos
-        setName('');
-        setAge('');
-        setDate('');
+      // Si todas las validaciones pasan, agregar el pasajero
+      addTask(name, ageNum, antelacion);
+      Swal.fire('Éxito', 'Pasajero registrado correctamente.', 'success');
+      // Limpiar campos
+      setName('');
+      setAge('');
+      setAntelacion('');
     }
-};
+  };
   return (
-    <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}
+    >
       <label htmlFor="task-name">Nombre del pasajero:</label>
       <input
         type="text"
@@ -52,7 +53,7 @@ function Form({ addTask }) {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      
+
       <label htmlFor="age">Edad del pasajero:</label>
       <input
         type="number"
@@ -60,15 +61,15 @@ function Form({ addTask }) {
         value={age}
         onChange={(e) => setAge(e.target.value)}
       />
-      
-      <label htmlFor="date">Fecha del viaje:</label>
+
+      <label htmlFor="antelacion">Antelación:</label>
       <input
-        type="date"
-        id="date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
+        type="number"
+        id="antelacion"
+        value={antelacion}
+        onChange={(e) => setAntelacion(e.target.value)}
       />
-      
+
       <button
         type="submit"
         style={{
@@ -80,12 +81,11 @@ function Form({ addTask }) {
           borderRadius: '5px',
           cursor: 'pointer',
         }}
-      >REGISTRAR BOLETO</button>
+      >
+        REGISTRAR BOLETO
+      </button>
     </form>
   );
 }
 
 export default Form;
-
-
-
